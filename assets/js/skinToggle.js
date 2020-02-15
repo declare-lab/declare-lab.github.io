637de6b12194d51293f8798618c4c1bd83af8fcd
Navonil Majumder <n.majumder.2009@gmail.com>
2020-02-14 18:03:14 -0600

    Revert "navbar_logo as darkmode toggle"
    This reverts commit 3bd86cce8fc3b0f2b32452cfac1db66285299f30.

--- assets/js/skinToggle.js
+++ assets/js/skinToggle.js
@@ -11,10 +11,9 @@ function disableDarkMode() {
     var link = document.getElementById('dark-toggle-link');
     let temp =  link.href.replace('main', 'light');
     link.href =  temp.replace('dark', 'light');
-    var r = document.getElementById('dark-toggle');
-    r.classList.remove('dark');
-    r.classList.add('light');
-    var e = document.getElementById('big-home-logo');
+    var e = document.getElementById('dark-toggle').children[0];
+    e.src = e.src.replace('dark', 'light');
+    e = document.getElementById('big-home-logo');
     if (e !== null)
         e.src = e.src.replace('dark', 'light');
     localStorage.setItem("darkMode", null);
@@ -24,10 +23,9 @@ function enableDarkMode() {
     var link = document.getElementById('dark-toggle-link');
     let temp =  link.href.replace('main', 'dark');
     link.href =  temp.replace('light', 'dark');
-    var r = document.getElementById('dark-toggle');
-    r.classList.remove('light');
-    r.classList.add('dark');
-    var e = document.getElementById('big-home-logo');
+    var e = document.getElementById('dark-toggle').children[0];
+    e.src = e.src.replace('light', 'dark');
+    e = document.getElementById('big-home-logo');
     if (e !== null)
         e.src = e.src.replace('light', 'dark');
     localStorage.setItem("darkMode", "enabled");
